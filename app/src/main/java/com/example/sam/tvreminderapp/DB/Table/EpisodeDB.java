174ab9b0006f779df8283b146779688c0cff0adf
Chirat Angelo <angeloochirat@gmail.com>
2018-01-09 19:48:56 +0200

    New files (empty)

---
+++ app/src/main/java/com/example/sam/tvreminderapp/DB/Table/EpisodeDB.java
@@ -0,0 +1,8 @@
+package com.example.sam.tvreminderapp.DB.Table;
+
+/**
+ * Created by Angelo on 09/01/2018.
+ */
+
+public class EpisodeDB {
+}
---
+++ app/src/main/java/com/example/sam/tvreminderapp/Object/Season.java
@@ -0,0 +1,8 @@
+package com.example.sam.tvreminderapp.Object;
+
+/**
+ * Created by Angelo on 09/01/2018.
+ */
+
+public class Season {
+}
