f9ad2f3c4111c0a647ade709e4a098441ccdc6df
Samuel Jeune <sam-j@hotmail.fr>
2018-01-09 13:47:01 +0200

    Modification of the display of search result

--- app/src/main/java/com/example/sam/tvreminderapp/SearchActivityPackage/searchResultViewHolder.java
+++ app/src/main/java/com/example/sam/tvreminderapp/SearchActivityPackage/searchResultViewHolder.java
@@ -2,9 +2,11 @@ package com.example.sam.tvreminderapp.SearchActivityPackage;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.sam.tvreminderapp.R;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,22 +17,25 @@ import org.json.JSONObject;
 
 public class searchResultViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView textViewView;
+        private TextView titleTextView;
         private TextView yearTextView;
         private TextView typeTextView;
+        private View view;
 
         public searchResultViewHolder(View itemView) {
             super(itemView);
-            textViewView = itemView.findViewById(R.id.textview);
-            yearTextView = itemView.findViewById(R.id.yeartextview);
-            typeTextView = itemView.findViewById(R.id.typetextView);
+            view = itemView;
+            titleTextView = itemView.findViewById(R.id.searchtitle_item);
+            yearTextView = itemView.findViewById(R.id.searchyear_item);
         }
 
         public void bind(final JSONObject myObject, final searchResultAdapter.OnItemClickListener listener){
             try {
-                textViewView.setText(myObject.getString("Title"));
+                titleTextView.setText(myObject.getString("Title"));
                 yearTextView.setText(myObject.getString("Year"));
-                typeTextView.setText(myObject.getString("Type"));
+                //typeTextView.setText(myObject.getString("Type"));
+                Picasso.with(view.getContext()).load(myObject.getString("Poster")).into((ImageView) view.findViewById(R.id.searchposter));
+
                 itemView.setOnClickListener(new View.OnClickListener() {
                     @Override public void onClick(View v) {
                         listener.onItemClick(myObject);
